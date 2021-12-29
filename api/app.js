6452cd14099d1585a64860d4ffb9b86ce1082fdd
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-12-29 13:19:08 -0500

    Connect to MongoDB

--- api/app.js
+++ api/app.js
@@ -1,13 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+require('dotenv').config();
+
+// Setting up Mongoose and MongoDB
+const mongoose = require('mongoose');                                                                                             //Import the mongoose module
+const dbUserName = process.env.MONGODB_USERNAME;
+const dbPassword = process.env.MONGODB_PASSWORD;
+const dbName = process.env.MONGODB_DBNAME;
+const mongoDB = `mongodb+srv://${dbUserName}:${dbPassword}@cluster0.t5chb.mongodb.net/${dbName}?retryWrites=true&w=majority`;     // Store the DB origin
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});                                                     //Set up default mongoose connection
+const db = mongoose.connection;                                                                                                   //Get the default connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));                                                         //Bind connection to error event (to get notification of connection errors)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
