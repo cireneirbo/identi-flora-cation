a23728f30118f59ce4ecf6fa02570b3456a8bc8c
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-12-30 22:43:09 -0500

    Display array in React

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import axios from 'axios';
-import async from 'async';
+// import async from 'async';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-
+function App() {
+//class App extends Component {
+/*
   constructor(props) {
     super(props);
     this.state = { };
-    /*this.moreArr = "unchaged";*/
+    
   }
-  
+
   callAPI() {
     axios.get("http://localhost:9000/")
     .then(res => {
       const data = res.data;
       this.setState(data);
       console.log(this.state);
-      /*this.moreArr = this.state.moreInfo.map((numList) => 
-        <p>{numList}</p>
-      ); */
+      
     });
   }
 
   componentDidMount() {
       this.callAPI();
-      
   }
+*/
+  
+  const [productsList, setProductsList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  
-  
-  render() {
-    //console.log(this.state);
+  useEffect(() => {
+    fetch("http://localhost:9000/")
+      .then((res) => res.json())
+      .then((data) => setProductsList([data]))
+      .then(setIsLoading(false));
+  }, []);
+
+
+  //render() {
+    // const arr = this.state.moreInfo;
     return (
       <div className="App">
         <header className="App-header">
@@ -49,30 +57,27 @@ class App extends Component {
             Learn React
           </a>
         </header>
-        <p>{this.state.title}</p>
-        {/* <p>{this.moreArr}</p> */}
+        {/* <p>{this.state.title}</p> */}
         
+        {/* <ol>
+          {arr.map(info => (
+            <li key={info}>{info}</li>
+          ))}
+        </ol> */}
+        {/* {isLoading ? (
+          <div className='spinner-border text-primary' role='status'>
+            {' '}
+            <span className='sr-only'>Loading...</span>{' '}
+          </div>
+        ) :  */}
+        {(
+          productsList.map(product => {
+                <p key={product.id}>{product.name}</p>
+          })}
+        // )}
       </div>
     );
-  }
-  /*return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );*/
+  //}
 }
 
 export default App;
