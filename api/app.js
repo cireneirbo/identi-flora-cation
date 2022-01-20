95d3dd259e58892fc804d050a310ed0d15c2d889
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-01-20 16:25:16 -0500

    Rename image files

--- api/app.js
+++ api/app.js
@@ -10,7 +10,7 @@ const catalogRouter = require('./routes/catalog');
 
 const app = express();
 require('dotenv').config();
-
+/*
 // Setting up Mongoose and MongoDB
 const mongoose = require('mongoose');                                                                                             //Import the mongoose module
 const dbUserName = process.env.MONGODB_USERNAME;
@@ -20,7 +20,7 @@ const mongoDB = `mongodb+srv://${dbUserName}:${dbPassword}@cluster0.t5chb.mongod
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});                                                     //Set up default mongoose connection
 const db = mongoose.connection;                                                                                                   //Get the default connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));                                                         //Bind connection to error event (to get notification of connection errors)
-
+*/
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
