1bc990d70decaf28eddeaad5c9a127539f1dd189
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-01-26 14:43:03 -0500

    Remove unnecessary code

--- api/controllers/shrubController.js
+++ api/controllers/shrubController.js
@@ -1,6 +1,4 @@
 // Import libraries
-const { body,validationResult } = require('express-validator');
-const async = require('async');
 
 // Import project files
 const Shrub = require('../models/shrub');
@@ -17,10 +15,8 @@ exports.shrub_list = function(req, res, next) {
     
     const list_shrubs = Shrub.shrubs;
     
-    //res.render('shrub_list', { title: 'Shrub List', shrub_list: list_shrubs} );
-    res.send({ title: 'Shrub List', shrub_list: list_shrubs} );
+    res.send( { title: 'Shrub List', shrub_list: list_shrubs} );
         
-  
 };
 
 // Display detail page for a specific Shrub.
@@ -39,51 +35,9 @@ exports.shrub_detail = function(req, res, next) {
         }
     }
     
-    //res.render('shrub_detail', { title: 'Shrub Detail', shrub: shrub } );
-    res.send({ title: 'Shrub Detail', shrub: shrub } );
-    
-
-};
-/*
-// Display Shrub delete form on GET.
-exports.shrub_delete_get = function(req, res, next) {
-
-    async.parallel({
-        shrub: function(callback) {
-            Shrub.findById(req.params.id).exec(callback)
-        },
-    }, function(err, results) {
-        if (err) { return next(err); }
-        if (results.shrub==null) { // No results.
-            res.redirect('/catalog/shrubs');
-        }
-        // Successful, so render.
-        res.render('shrub_delete', { title: 'Delete Shrub', shrub: results.shrub } );
-    });
-
-};
-
-// Handle Shrub delete on POST.
-exports.shrub_delete_post = function(req, res, next) {
-
-    async.parallel({
-        shrub: function(callback) {
-          Shrub.findById(req.body.shrubid).exec(callback)
-        },
-    }, function(err, results) {
-        if (err) { return next(err); }
-        // Success
-        // Delete object and redirect to the list of shrubs.
-        Shrub.findByIdAndRemove(req.body.shrubid, function deleteShrub(err) {
-            if (err) { return next(err); }
-            // Success - go to shrub list
-            res.redirect('/catalog/shrubs')
-        })
-        
-    });
+    res.send( { title: 'Shrub Detail', shrub: shrub } );
     
 };
-*/
 
 // /catalog/about page
 exports.about = function(req, res) {
@@ -150,7 +104,7 @@ exports.shrub_search_get = function(req, res, next) {
             if(!searchedShrubs.includes(Shrub.shrubs[i])) {
                 searchedShrubs.push(Shrub.shrubs[i]);
                 message = "We have found the shrubs you are looking for!";
-                console.log("Added: " + Shrub.shrubs[i].common_name);
+                //console.log("Added: " + Shrub.shrubs[i].common_name);
             }
         }
         //bark color
@@ -159,7 +113,7 @@ exports.shrub_search_get = function(req, res, next) {
             if(!searchedShrubs.includes(Shrub.shrubs[i])) {
                 searchedShrubs.push(Shrub.shrubs[i]);
                 message = "We have found the shrubs you are looking for!";
-                console.log("Added: " + Shrub.shrubs[i].common_name);
+                //console.log("Added: " + Shrub.shrubs[i].common_name);
             }
         }
         //stem color
@@ -168,7 +122,7 @@ exports.shrub_search_get = function(req, res, next) {
             if(!searchedShrubs.includes(Shrub.shrubs[i])) {
                 searchedShrubs.push(Shrub.shrubs[i]);
                 message = "We have found the shrubs you are looking for!";
-                console.log("Added: " + Shrub.shrubs[i].common_name);
+                //console.log("Added: " + Shrub.shrubs[i].common_name);
             }
         }
         //fruit color
@@ -177,7 +131,7 @@ exports.shrub_search_get = function(req, res, next) {
             if(!searchedShrubs.includes(Shrub.shrubs[i])) {
                 searchedShrubs.push(Shrub.shrubs[i]);
                 message = "We have found the shrubs you are looking for!";
-                console.log("Added: " + Shrub.shrubs[i].common_name);
+                //console.log("Added: " + Shrub.shrubs[i].common_name);
             }
         }
         //flower color
@@ -186,7 +140,7 @@ exports.shrub_search_get = function(req, res, next) {
             if(!searchedShrubs.includes(Shrub.shrubs[i])) {
                 searchedShrubs.push(Shrub.shrubs[i]);
                 message = "We have found the shrubs you are looking for!";
-                console.log("Added: " + Shrub.shrubs[i].common_name);
+                //console.log("Added: " + Shrub.shrubs[i].common_name);
             }
         }
         //plant type
@@ -195,7 +149,7 @@ exports.shrub_search_get = function(req, res, next) {
             if(!searchedShrubs.includes(Shrub.shrubs[i])) {
                 searchedShrubs.push(Shrub.shrubs[i]);
                 message = "We have found the shrubs you are looking for!";
-                console.log("Added: " + Shrub.shrubs[i].common_name);
+                //console.log("Added: " + Shrub.shrubs[i].common_name);
             }
         }
         // plant shapes
@@ -204,7 +158,7 @@ exports.shrub_search_get = function(req, res, next) {
             if(!searchedShrubs.includes(Shrub.shrubs[i])) {
                 searchedShrubs.push(Shrub.shrubs[i]);
                 message = "We have found the shrubs you are looking for!";
-                console.log("Added: " + Shrub.shrubs[i].common_name);
+                //console.log("Added: " + Shrub.shrubs[i].common_name);
             }
         }
         // leaf dimensions
@@ -213,7 +167,7 @@ exports.shrub_search_get = function(req, res, next) {
             if(!searchedShrubs.includes(Shrub.shrubs[i])) {
                 searchedShrubs.push(Shrub.shrubs[i]);
                 message = "We have found the shrubs you are looking for!";
-                console.log("Added: " + Shrub.shrubs[i].common_name);
+                //console.log("Added: " + Shrub.shrubs[i].common_name);
             }
         }
         // flower dimensions
@@ -222,12 +176,12 @@ exports.shrub_search_get = function(req, res, next) {
             if(!searchedShrubs.includes(Shrub.shrubs[i])) {
                 searchedShrubs.push(Shrub.shrubs[i]);
                 message = "We have found the shrubs you are looking for!";
-                console.log("Added: " + Shrub.shrubs[i].common_name);
+                //console.log("Added: " + Shrub.shrubs[i].common_name);
             }
         }
         else {
-            console.log(Shrub.shrubs[i].common_name);
-            console.log("No match: " + req.params);
+            //console.log(Shrub.shrubs[i].common_name);
+            //console.log("No match: " + req.params);
         }
 
     }
@@ -235,79 +189,7 @@ exports.shrub_search_get = function(req, res, next) {
     if(searchedShrubs.length == 0) {
         message = "We could not find any matches...";
     }
-    //console.log(searchParamsValues)
-    //console.log(searchedShrubs)
-            
-    //res.render('shrub_form', { title: 'Search', shrub_list: list_shrubs} );
+    
     res.send( { message: message, shrub_list: searchedShrubs} );
         
-
 };
-/*
-// Handle Shrub search on POST.
-exports.shrub_search_post = [
-
-    // Validate and sanitise fields.
-    body('plant_type').exists(),
-    body('plant_shape').exists(),
-    body('leaf_color').exists(),
-    body('leaf_type').exists(),
-    body('leaf_dimensions').exists(),
-    body('bark_color').exists(),
-    body('stem_color').exists(),
-    body('fruit_color').exists(),
-    body('flower_color').exists(),
-    body('flower_dimensions').exists(),
-    
-    // Process request after validation and sanitization.
-    (req, res, next) => {
-
-        // Extract the validation errors from a request.
-        const errors = validationResult(req);
-
-        
-        // Create a Shrub object with escaped/trimmed data and old id.
-        const shrub_search_list = { 
-            plant_type: req.body.plant_type,
-            plant_shape: req.body.plant_shape,
-            leaf_color: req.body.leaf_color,
-            leaf_type: req.body.leaf_type,
-            leaf_dimensions: req.body.leaf_dimensions,
-            bark_color: req.body.bark_color,
-            stem_color: req.body.stem_color,
-            fruit_color: req.body.fruit_color,
-            flower_color: req.body.flower_color,
-            flower_dimensions: req.body.flower_dimensions,
-        }
-        
-
-        let searchedData = [];
-
-        // Search for each shrub_search_list with an `$or` expression
-        Shrub.find({ $or: [
-            { leaf_color: shrub_search_list.leaf_color }, 
-            { plant_type: shrub_search_list.plant_type },
-            { plant_shape: shrub_search_list.plant_shape },
-            { leaf_type: shrub_search_list.leaf_type },
-            { leaf_dimensions: shrub_search_list.leaf_dimensions },
-            { bark_color: shrub_search_list.bark_color },
-            { stem_color: shrub_search_list.stem_color },
-            { fruit_color: shrub_search_list.fruit_color },
-            { flower_color: shrub_search_list.flower_color },
-            { flower_dimensions: shrub_search_list.flower_dimensions },
-        ]})
-        .sort([['common_name', 'ascending']])
-        .exec(function (err, list_shrubs) {
-            if (err) { return next(err); }
-            //Successful, so render
-                if (!searchedData.includes(list_shrubs)) {
-                    searchedData.push(list_shrubs);
-                }
-                    
-            res.render('search_results', { title: 'Search', /*shrub_search_list: shrub_search_list,*/ //searchedData: searchedData } );
-            //res.send({ /*shrub_search_list: shrub_search_list, shrub_list: list_shrubs,*/ searchedData: searchedData /*shrub_leaf_color: shrub_search_list.leaf_color*/ } );
-        //});
-
-    //}
-//];
-
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const router = express.Router();
-//const shrub_controller = require('../controllers/shrubController');
-
-/* GET home page. */
-//router.get('/', shrub_controller.shrub_list);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
